24bf02cc3ab8bb7787762261407a3189221c48f1
ThBo93 <thomas.bottoni@outlook.com>
2018-07-31 14:23:06 +0200

    maj requetes

--- Projet_Vol/src/main/java/sql/SQLRequest_Delete.java
+++ Projet_Vol/src/main/java/sql/SQLRequest_Delete.java
@@ -1,4 +1,10 @@
-package sql;
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import util.Closer;
+import util.Context;
 
 public class SQLRequest_Delete {
 	private final static String DELETE_CLIENT = "delete from client where id_client=?";
@@ -6,5 +12,21 @@ public class SQLRequest_Delete {
 	private final static String DELETE_VOL = "delete from vol where id_vol=?";
 	private final static String DELETE_RESERVATION = "delete from reservation where id_reservation=?";
 	private final static String DELETE_PASSAGER = "delete from passager where id_passager=?";
+
+	// SUPPRIMER UN CLIENT
+	public int deleteClient(Context ctx, int id_client) {
+		int retour = 0;
+		PreparedStatement ps = null;
+		try {
+			ps = ctx.getConnection().prepareStatement(DELETE_CLIENT);
+			ps.setInt(1, id_client);
+			retour = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Closer.closeStatement(ps);
+		}
+		return retour;
+	}
 
 }
--- Projet_Vol/src/main/java/sql/SQLRequest_Insert.java
+++ Projet_Vol/src/main/java/sql/SQLRequest_Insert.java
@@ -1,4 +1,6 @@
-package sql;
+package dao;
+
+import java.sql.Date;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,13 +10,15 @@ import util.Closer;
 import util.Context;
 
 public class SQLRequest_Insert {
-	private final static String INSERT_CLIENT = "insert into client(id_client,nom,numeroTel,numeroFax,email) values(nextval('seq_client'),?,?,?,?)";
+	private final static String INSERT_CLIENT = "insert into client(id_client,nom,numeroTel,numeroFax,email,adresse,cp,ville,pays) values(nextval('seq_client'),?,?,?,?,?,?,?,?)";
 	private final static String INSERT_LOGIN = "insert into login(id_login,login,motDePasse,admin) values('seq_login',?,?,?)";
-	private final static String INSERT_VOL = "insert into vol(id_vol,date_Depart,date_Arrivee,heure_Depart,heure_Arrivee) values(?,?,?,?,?)";
+	private final static String INSERT_VOL = "insert into vol(id_vol,date_Depart,date_Arrivee,heure_Depart,heure_Arrivee) values('seq_vol',?,?,?,?)";
 	private final static String INSERT_RESERVATION = "insert into reservation(id_reservation,date,numero) values(nextval('seq_reservation'),?,?)";
-	private final static String INSERT_PASSAGER = "insert into passager(id_passager,nom,prenom) values(nextval('seq_passager'),?,?)";
+	private final static String INSERT_PASSAGER = "insert into passager(id_passager,nom_passager,prenom_passager,adresse_passager,cp_passager,ville_passager,pays_passager) values(nextval('seq_passager'),?,?,?,?,?,?)";
 
-	public int insertClient(Context ctx, String nom, Integer numeroTel, Integer numeroFax, String email) {
+	// INSERER UN CLIENT
+	public int insertClient(Context ctx, String nom, Integer numeroTel, Integer numeroFax, String email, String adresse,
+			String cp, String ville, String pays) {
 		PreparedStatement ps = null;
 		Statement st = null;
 		int numeroGenere = -1;
@@ -25,9 +29,121 @@ public class SQLRequest_Insert {
 			ps.setInt(2, numeroTel);
 			ps.setInt(3, numeroFax);
 			ps.setString(4, email);
+			ps.setString(5, adresse);
+			ps.setString(6, cp);
+			ps.setString(7, ville);
+			ps.setString(8, pays);
 			ps.executeUpdate();
 			st = ctx.getConnection().createStatement();
 			rs = st.executeQuery("select currval('seq_client')");
+			if (rs.next()) {
+				numeroGenere = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			Closer.closeStatement(st);
+			Closer.closeStatement(ps);
+		}
+		return numeroGenere;
+	}
+
+	// INSERER UN LOGIN
+	public int insertLogin(Context ctx, String login, String motDePasse, Boolean admin) {
+		PreparedStatement ps = null;
+		Statement st = null;
+		int numeroGenere = -1;
+		ResultSet rs = null;
+		try {
+			ps = ctx.getConnection().prepareStatement(INSERT_LOGIN);
+			ps.setString(1, login);
+			ps.setString(2, motDePasse);
+			ps.setBoolean(3, admin);
+			ps.executeUpdate();
+			st = ctx.getConnection().createStatement();
+			rs = st.executeQuery("select currval('seq_login')");
+			if (rs.next()) {
+				numeroGenere = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			Closer.closeStatement(st);
+			Closer.closeStatement(ps);
+		}
+		return numeroGenere;
+	}
+
+	// INSERER UN VOL
+	public int insertVol(Context ctx, Date date_Depart, Date date_Arrivee, Date heure_Depart, Date heure_Arrivee) {
+		PreparedStatement ps = null;
+		Statement st = null;
+		int numeroGenere = -1;
+		ResultSet rs = null;
+		try {
+			ps = ctx.getConnection().prepareStatement(INSERT_VOL);
+			ps.setDate(1, date_Depart);
+			ps.setDate(2, date_Arrivee);
+			ps.setDate(3, heure_Depart);
+			ps.setDate(4, heure_Arrivee);
+			ps.executeUpdate();
+			st = ctx.getConnection().createStatement();
+			rs = st.executeQuery("select currval('seq_vol')");
+			if (rs.next()) {
+				numeroGenere = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			Closer.closeStatement(st);
+			Closer.closeStatement(ps);
+		}
+		return numeroGenere;
+	}
+
+	// INSERER UNE RESERVATION
+	public int insertReservation(Context ctx, Date date, Integer numero) {
+		PreparedStatement ps = null;
+		Statement st = null;
+		int numeroGenere = -1;
+		ResultSet rs = null;
+		try {
+			ps = ctx.getConnection().prepareStatement(INSERT_RESERVATION);
+			ps.setDate(1, date);
+			ps.setInt(2, numero);
+			ps.executeUpdate();
+			st = ctx.getConnection().createStatement();
+			rs = st.executeQuery("select currval('seq_reservation')");
+			if (rs.next()) {
+				numeroGenere = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			Closer.closeStatement(st);
+			Closer.closeStatement(ps);
+		}
+		return numeroGenere;
+	}
+
+	// INSERER UN PASSAGER
+	public int insertPassager(Context ctx, String nom_passager, String prenom_passager, String adresse_passager, String cp_passager,
+			String ville_passager, String pays_passager) {
+		PreparedStatement ps = null;
+		Statement st = null;
+		int numeroGenere = -1;
+		ResultSet rs = null;
+		try {
+			ps = ctx.getConnection().prepareStatement(INSERT_CLIENT);
+			ps.setString(1, nom_passager);
+			ps.setString(2, prenom_passager);
+			ps.setString(3, adresse_passager);
+			ps.setString(4, cp_passager);
+			ps.setString(6, ville_passager);
+			ps.setString(7, pays_passager);
+			ps.executeUpdate();
+			st = ctx.getConnection().createStatement();
+			rs = st.executeQuery("select currval('seq_passager')");
 			if (rs.next()) {
 				numeroGenere = rs.getInt(1);
 			}
